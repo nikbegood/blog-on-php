4a84fad5431754d6f8178123c74135031c46d2c1
nikbegood <nikita.tsymbalenko@mail.ru>
2018-12-18 19:53:25 +0300

    Add admin routes

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,22 @@
 <?php
 return array(
 
+	// main 
+
 	'news/([0-9]+)' => 'news/view/$1',
 	//'news' => 'news/index', 
 	'index.php' => 'news/index', // actionIndex in NewsController
     '' => 'news/index',
+
+    'about' => 'news/about',
+
+    'contact' => 'news/contact',
+    // admin Actions in AdminController
+    'post' => 'admin/post',
+    'login' => 'admin/login',
+    'logout' => 'admin/logout',
+    'edit' => 'admin/edit',
+    'delete' => 'admin/delete',
 
 
 	);
